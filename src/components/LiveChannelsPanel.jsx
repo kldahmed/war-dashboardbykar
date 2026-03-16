d414d67ea511ca208c3a332b143242e60103f180
kldahmed <realxlaaad@gmail.com>
2026-03-16 07:07:21 +0400

    Update LiveChannelsPanel.jsx

--- src/components/LiveChannelsPanel.jsx
+++ src/components/LiveChannelsPanel.jsx
@@ -1,75 +1,42 @@
 import React, { useState } from "react";
 
 const CHANNELS = [
-{
-id: "aljazeera",
-name: "الجزيرة مباشر",
-country: "Qatar",
-youtube: "https://www.youtube.com/embed/bNyUyrR0PHo"
-},
-
-{
-id: "skynewsarabia",
-name: "سكاي نيوز عربية",
-country: "UAE",
-youtube: "https://www.youtube.com/embed/8d0qF6V6H1Y"
-},
-
-{
-id: "france24arabic",
-name: "فرانس 24 عربي",
-country: "International",
-youtube: "https://www.youtube.com/embed/QlTQhQL5M4E"
-},
-
-{
-id: "bbc",
-name: "BBC News",
-country: "International",
-youtube: "https://www.youtube.com/embed/9Auq9mYxFEE"
-},
-
-{
-id: "dw",
-name: "DW News",
-country: "International",
-youtube: "https://www.youtube.com/embed/NvqKZHpKs-g"
-},
-
-{
-id: "trtworld",
-name: "TRT World",
-country: "International",
-youtube: "https://www.youtube.com/embed/Zw4U0p1YfEo"
-},
-
-{
-id: "euronews",
-name: "Euronews",
-country: "International",
-youtube: "https://www.youtube.com/embed/pykpO5kQJ98"
-},
-
-{
-id: "cnn",
-name: "CNN International",
-country: "International",
-youtube: "https://www.youtube.com/embed/9hG0qJ3i0x0"
-},
-
-{
-id: "bloomberg",
-name: "Bloomberg",
-country: "International",
-youtube: "https://www.youtube.com/embed/dp8PhLsUcFE"
-},
-
-{
-id: "fox",
-name: "Fox News",
-country: "International",
-youtube: "https://www.youtube.com/embed/lAKV7Vq2c2E"
-}
+  {
+    id: "aljazeera",
+    name: "الجزيرة مباشر",
+    country: "Qatar",
+    type: "embed",
+    youtubeId: "bNyUyrR0PHo",
+    externalUrl: "https://www.youtube.com/@aljazeeraarabic/live"
+  },
+  {
+    id: "skynewsarabia",
+    name: "سكاي نيوز عربية",
+    country: "UAE",
+    type: "external",
+    externalUrl: "https://www.youtube.com/@skynewsarabia/live"
+  },
+  {
+    id: "france24arabic",
+    name: "فرانس 24 بالعربي",
+    country: "International",
+    type: "external",
+    externalUrl: "https://www.youtube.com/@FRANCE24Arabic/live"
+  },
+  {
+    id: "bbc",
+    name: "BBC News",
+    country: "International",
+    type: "external",
+    externalUrl: "https://www.youtube.com/@BBCNews/live"
+  },
+  {
+    id: "dw",
+    name: "DW News",
+    country: "International",
+    type: "external",
+    externalUrl: "https://www.youtube.com/@dwnews/live"
+  }
 ];
 
 export default function LiveChannelsPanel(){
